4edc6fad049d3c055995e92fd2d64111164475f0
Varun Nagaraj <varun.naagaraj@gmail.com>
2018-12-19 21:13:53 -0500

    fixed issue

--- butterfly_counting.py
+++ butterfly_counting.py
@@ -5,59 +5,63 @@ Prerequisite: Graphs should be undirected unweighted and bipartite in nature. Pr
 
 import networkx as nx
 import matplotlib.pyplot as plt
+import datetime as dt
+import math
 
-fb_data = nx.read_edgelist('facebook_combined.txt', create_using=nx.Graph())
+start_time = dt.datetime.now()
+fb_data = nx.read_edgelist('board_members.rtf', create_using=nx.Graph())
 print(nx.info(fb_data))
 
 edges = fb_data.edges()
 
-top_nodes = []
-bottom_nodes = []
+top_nodes = set()
 for i in edges:
-    top_nodes.append(i[0])
-    bottom_nodes.append(i[1])
-    
-def uniq(lst):
-    last = object()
-    for item in lst:
-        if item == last:
-            continue
-        yield item
-        last = item
-
-def sort_and_deduplicate(l):
-    return list(uniq(sorted(l, reverse=True)))
-    
-butterfly = list()
-
-
-def second_level_neighbors(second_level_nbrs, i, j, k):
-    for l in second_level_nbrs:
-        if l == j:
-            continue
-        if i in fb_data.neighbors(l):
-            butterfly_content = sorted([str(i), str(j), str(k), str(l)])
-            butterfly.append(butterfly_content)
-
-
-def current_node_neighbors(current_nbrs, i):
-    global j, k, second_level_nbrs
-    for j in set(current_nbrs):
-        bottom_node_nbrs = fb_data.neighbors(j)
-        for k in bottom_node_nbrs:
-            if k == i:
-                continue
-            second_level_nbrs = fb_data.neighbors(k)
-            second_level_neighbors(second_level_nbrs, i, j, k)
+    top_nodes.add(i[0])
 
 
 def find_butterflies():
+    complete_dict_of_butterflies = dict()
+    number_of_butterflies = 0
     for i in set(top_nodes):
+        complete_dict_of_butterflies.update({i:[]})
+        butterfly = dict()
         current_nbrs = fb_data.neighbors(i)
-        current_node_neighbors(current_nbrs, i)
+        for nbrs in current_nbrs:
+            for w in fb_data.neighbors(nbrs):
+                try:
+                    butterfly[w] += 1
+                except:
+                    butterfly[w] = 1
+                complete_dict_of_butterflies[i] = list(set(complete_dict_of_butterflies[i] + [w]))
+        for w in butterfly.keys():
+            total = 0
+            if butterfly[w] > 0:
+                if butterfly[w] - 2 > 0:
+                    total = math.factorial(butterfly[w])/ (2* math.factorial(butterfly[w] - 2))
+                number_of_butterflies += total
+    return number_of_butterflies, complete_dict_of_butterflies
 
-find_butterflies()
-print("The butterflies are: ",sort_and_deduplicate(butterfly))
 
-nx.draw(fb_data, with_labels=True)
+def vertex_distribution(complete_dict):
+    percentage = dict()
+    for key, value in complete_dict.items():
+        for i in value:
+            try:
+                percentage[i] += 1
+            except:
+                percentage[i] = 1
+    return percentage
+
+
+final_bfc, butterfly = find_butterflies()
+print("number of butterflies are:{}".format(final_bfc/2))
+print("Runtime is :{}".format(dt.datetime.now()-start_time))
+v_dist = vertex_distribution(butterfly)
+print(v_dist)
+names = list(v_dist.keys())
+values = list(v_dist.values())
+
+plt.bar(range(len(v_dist)),values,tick_label=names)
+plt.savefig('vertex_distribution.png')
 plt.show()
+
