a824277b5a6d35010e0ba8b665b05df4ec50efd0
Varun Nagaraj <varun.naagaraj@gmail.com>
2019-01-16 22:26:49 -0500

    Revised code of exact BFC

--- butterfly_counting.py
+++ butterfly_counting.py
@@ -2,44 +2,80 @@
 This is a piece of code which tries to find the 2,2 bicliques (butterflies) in the Large Network datasets.
 Prerequisite: Graphs should be undirected unweighted and bipartite in nature. Preferably should not contain self loops.
 """
-
 import networkx as nx
 import matplotlib.pyplot as plt
 import datetime as dt
-import math
+import pandas as pd
+
 
 start_time = dt.datetime.now()
-fb_data = nx.read_edgelist('board_members.rtf', create_using=nx.Graph())
-print(nx.info(fb_data))
+data = pd.read_csv('board_members.txt', sep=" ", header=None, dtype=str)
+data=data.dropna(how='all')
+data = data.dropna()
+max_value = 0
+for item in data[0]:
+    if int(item) > max_value:
+        max_value = int(item)
 
-edges = fb_data.edges()
+vert_L = set()
+vert_R = set()
+for i, j in data.iterrows():
+    if j[0] == '}' or j[1] == '}':
+        continue
+    vert_L.add(j[0])
+    vert_R.add(str(int(j[1])+ max_value))
 
-top_nodes = set()
-for i in edges:
-    top_nodes.add(i[0])
+B = nx.Graph()
+B.add_nodes_from(vert_L, bipartite=0)
+B.add_nodes_from(vert_R, bipartite=1)
+for row in data.iterrows():
+    B.add_edge(row[1][0], str(int(row[1][1])+ max_value))
+
+max = 0
+for i in vert_L:
+    if i > max:
+        max = int(i)
+
+max2 = 0
+for i in vert_R:
+    if i > max2:
+        max2 = int(i)
+
+hashmap_c = [0]* (max+max2)
+aux_array = [0]* (max+max2)
 
 
-def find_butterflies():
+def exact_bfc():
     complete_dict_of_butterflies = dict()
-    number_of_butterflies = 0
-    for i in set(top_nodes):
-        complete_dict_of_butterflies.update({i:[]})
-        butterfly = dict()
-        current_nbrs = fb_data.neighbors(i)
-        for nbrs in current_nbrs:
-            for w in fb_data.neighbors(nbrs):
-                try:
-                    butterfly[w] += 1
-                except:
-                    butterfly[w] = 1
-                complete_dict_of_butterflies[i] = list(set(complete_dict_of_butterflies[i] + [w]))
-        for w in butterfly.keys():
-            total = 0
-            if butterfly[w] > 0:
-                if butterfly[w] - 2 > 0:
-                    total = math.factorial(butterfly[w])/ (2* math.factorial(butterfly[w] - 2))
-                number_of_butterflies += total
-    return number_of_butterflies, complete_dict_of_butterflies
+    bf_count = 0
+    edge_list = list()
+    for v in vert_L:
+        complete_dict_of_butterflies.update({v:0})
+        index = 0
+        for neighbor in B.neighbors(v):
+            two_hop_neighbors = B.neighbors(neighbor)
+            for thn in two_hop_neighbors:
+                if int(v) > int(thn):
+                    bf_count += hashmap_c[int(thn)]
+                    hashmap_c[int(thn)] += 1
+                    if hashmap_c[int(thn)] == 1:
+                        aux_array[index] = int(thn)
+                        index += 1
+                        temp = sorted([int(v), int(neighbor)])
+                        temp = (str(temp[0]), str(temp[1]))
+                        temp1 = sorted([int(neighbor), int(thn)])
+                        temp1 = (str(temp1[0]), str(temp1[1]))
+                        edge_list.append((temp))
+                        edge_list.append((temp1))
+                else:
+                    break
+        complete_dict_of_butterflies[v] = index
+        for j in range(index+1):
+            hashmap_c[aux_array[j]] = 0
+    return sum(complete_dict_of_butterflies.values())/2, complete_dict_of_butterflies, edge_list
+
+bf_counted, complete_bfc, edge_dist = exact_bfc()
+print("Number of Butterflies counted is {}".format(bf_counted))
 
 
 def vertex_distribution(complete_dict):
@@ -53,15 +89,36 @@ def vertex_distribution(complete_dict):
     return percentage
 
 
-final_bfc, butterfly = find_butterflies()
-print("number of butterflies are:{}".format(final_bfc/2))
-print("Runtime is :{}".format(dt.datetime.now()-start_time))
-v_dist = vertex_distribution(butterfly)
-print(v_dist)
-names = list(v_dist.keys())
-values = list(v_dist.values())
+def edge_distribution(edges):
+    edge_dist_dict = {}
+    for item in edges:
+        if item in edge_dist_dict.keys() and (item[1], item[0]) not in edge_dist_dict.keys():
+            edge_dist_dict[item] += 1
+        else:
+            edge_dist_dict[item] = 1
 
-plt.bar(range(len(v_dist)),values,tick_label=names)
+    return edge_dist_dict
+
+
+names = list(complete_bfc.keys())
+values = list(complete_bfc.values())
+#
+fig1, ax1 = plt.subplots()
+result_occurrences = {i:complete_bfc.values().count(i) for i in complete_bfc.values()}
+result_occurrences.pop(0)
+ax1.bar(result_occurrences.keys(), result_occurrences.values(), color='g')
+plt.title('Vertex Distribution')
+plt.xlabel('Vertex Occurrences')
+plt.ylabel('Number of Butterflies associated with vertices')
 plt.savefig('vertex_distribution.png')
-plt.show()
 
+fig2, ax2 = plt.subplots()
+edge_occurrences = {i:edge_dist.count(i) for i in edge_dist}
+edge_result_occurrences = {i:edge_occurrences.values().count(i) for i in edge_occurrences.values()}
+ax2.bar(edge_result_occurrences.keys(), edge_result_occurrences.values(), color='b')
+plt.title('Edge Distribution')
+plt.xlabel('Edge Occurrences')
+plt.ylabel('Number of Butterflies associated with edges')
+plt.savefig('edge_distribution.png')
+
+print("Time taken is {}".format(dt.datetime.now() - start_time))
